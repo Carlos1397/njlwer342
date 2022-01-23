f0c20da3ac1ec71fe3adb1525f99af425b81baba
Carlos Viera <pisis_13_3@hotmail.com>
2022-01-22 22:51:47 -0500

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      items: ["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+      input: ""
+    }
+
+    this.updateInput = this.updateInput.bind(this)
+    this.anadeTarea = this.anadeTarea.bind(this)
+  }
+  updateInput(e) {
+    this.setState({ input: e.target.value })
+  }
+  anadeTarea(e) {
+    e.preventDefault();
+    this.setState({
+      items: this.state.items.concat(this.state.input),
+      input: ""
+    });
+  }
   render() {
+    const { items, input } = this.state
     return (
       <div className="wrapper">
-        <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
-          </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
-        </div>
+
+        <h3>Por hacer:</h3>
+        <ul className="todo">
+          {items.map((item, index) =>
+            <li key={index}>{item}</li>)}
+        </ul>
+        <form onSubmit={this.anadeTarea}>
+          <input type="text" id="new-task"
+            value={input}
+            onChange={this.updateInput}
+            placeholder="Ingresa una tarea y oprime Enter" />
+        </form>
+
+
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import reactTestRenderer from 'react-test-renderer/cjs/react-test-renderer.development';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
